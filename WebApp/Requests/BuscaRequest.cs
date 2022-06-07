4818de03de772592bc97b50ed79c0b438a174314
Wesley Alves de Albuquerque <wealbuquerque@yahoo.com.br>
2022-06-07 00:27:24 -0300

    Adicionado classes para entidades

---
+++ WebApp/Requests/BuscaRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class BuscaRequest
+    {
+        public string EntityName { get; set; }
+        public string FiltroBusca { get; set; }
+        public string Chave { get; set; }
+        public string AttributeName { get; set; }
+    }
+}
