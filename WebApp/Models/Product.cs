1e3c36a0b4a7cc2bab01b95ba3e800ba5ec71d04
Wilker Albuquerque <wilkeralbuquerque@msn.com>
2022-06-07 00:50:35 -0300

    Criando propriedades do Modelo Produto

--- WebApp/Models/Product.cs
+++ WebApp/Models/Product.cs
@@ -7,5 +7,10 @@ namespace WebApp.Models
 {
     public class Product
     {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string ProdutoID { get; set; }
+        public DateTime ValidoAte { get; set; }
+        public DateTime ValidoAPartir {get;set; }
     }
 }
